78aceddf2abaa2969a09946a68cb07cab0cf2be5
jaaeyoung3048 <jaeyoung3048@gmail.com>
2025-06-23 23:19:43 +0900

    feat(db): 데이터베이스 기본 컬럼 Mixin 추가 - PrimaryKeyMixin 추가 - BasicDateTimeMixin 추가

--- app/db/base.py
+++ app/db/base.py
@@ -1,5 +1,18 @@
-from sqlalchemy.orm import DeclarativeBase
+from datetime import datetime
+
+from sqlalchemy import DateTime
+from sqlalchemy.dialects.mysql import INTEGER
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 
 class Base(DeclarativeBase):
     pass
+
+
+class PrimaryKeyMixin:
+    id: Mapped[int] = mapped_column(INTEGER(unsigned=True), primary_key=True, autoincrement=True)  # type: ignore[no-untyped-call]
+
+
+class BasicDateTimeMixin:
+    created_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now)
+    updated_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now, onupdate=datetime.now)
